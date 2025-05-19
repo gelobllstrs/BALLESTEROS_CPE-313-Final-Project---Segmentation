e3cb0460463e76e78a70ce2f8ef7068594a15006
gelobllstrs <gelobllstrs@gmail.com>
2025-05-19 18:21:40 +0800

    Update app_segmentation.py

--- app_segmentation.py
+++ app_segmentation.py
@@ -25,7 +25,7 @@ uploaded_file = st.file_uploader("Upload an image.", type=["jpg", "jpeg", "png"]
 
 if uploaded_file:
     image = Image.open(uploaded_file).convert("RGB")
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
 
     with tempfile.NamedTemporaryFile(delete=False, suffix=".jpg") as temp_file:
         image.save(temp_file.name)
